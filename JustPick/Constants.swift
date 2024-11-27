62a77c2e3823bc9b39e82dd429f57557778c373b
jfelic <felicianojulian1@gmail.com>
2024-11-27 15:40:11 -0500

    Session screen basic layout complete

---
+++ JustPick/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  JustPick
+//
+//  Created by Julian on 11/27/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theaterRed = Color(red: 214/255, green: 72/255, blue: 72/255)
+    static let popcornYellow = Color(red: 255/255, green: 222/255, blue: 156/255)
+//    static let backgroundNavy = Color(red: 52/255, green: 67/255, blue: 85/255)
+    static let backgroundNavy = Color(red: 26/255, green: 47/255, blue: 57/255)
+}
--- JustPick/ContentView.swift
+++ JustPick/ContentView.swift
@@ -8,40 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    func signIn() -> Void{
-        print("Hello")
-    }
-    
     var body: some View {
-        VStack {
-            
-            Button(action: {
-                print("Host button tapped")
-            }) {
-                Text("Host")
-                    .font(.title)
+        NavigationStack {
+            VStack {
+                ZStack {
+                    Image("sign")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity)
+                    Text("JustPick")
+                        .font(.custom("AntonSC-Regular", size: 40))
+                }
+                
+                Spacer()
+                
+                VStack(spacing: 25) { // Buttons Column
+                    Button(action: {
+                        print("Host button tapped")
+                    }) {
+                        Text("Host a Session")
+                            .font(.custom("AfacadFlux-VariableFont_slnt,wght", size: 30))
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 60)
+                            .background(Color.theaterRed)
+                            .foregroundColor(Color.popcornYellow)
+                            .cornerRadius(15)
+                    }
+                    .padding(.horizontal)
+                    
+                    Button(action: {
+                        print("Join button tapped")
+                    }) {
+                        Text("Join a Session")
+                            .font(.custom("AfacadFlux-VariableFont_slnt,wght", size: 30))
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 60)
+                            .background(Color.theaterRed)
+                            .foregroundColor(Color.popcornYellow)
+                            .cornerRadius(15)
+                    }
+                    .padding(.horizontal)
+                }
+                .offset(y: 200)
+                
+                Image("popcorn")
+                    .resizable()
                     .frame(maxWidth: .infinity)
-                    .frame(height: 60)
-                    .background(Color.green)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(15)
+                    .ignoresSafeArea(edges: .bottom)
+                    .offset(y: 300)
             }
-            .padding(.horizontal)
-            
-            Button(action: {
-                print("Join button tapped")
-            }) {
-                Text("Join")
-                    .font(.title)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 60)
-                    .background(Color.blue)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(15)
-            }
-            .padding(.horizontal)
+            .padding()
+            .background(Color.backgroundNavy)
         }
-        .padding()
     }
 }
 
